0345cebdfd9270c3af4677b529dc8cee28c72f4d
GEKKO7E <danishwaraeka@gmail.com>
2025-10-21 08:37:15 +0700

    ini adalah kalkulator sederhana

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
-// Nama file: Kalkulator.java
-
 public class Main {
 
     public static void main(String[] args) {
-        // 1. Deklarasi Variabel
-        // 'int' digunakan untuk menyimpan bilangan bulat.
         int angkaPertama = 10;
         int angkaKedua = 25;
         int hasil;
 
-        // 2. Proses Penjumlahan
-        // Menjumlahkan isi dari dua variabel dan menyimpannya di variabel 'hasil'.
         hasil = angkaPertama + angkaKedua;
 
-        // 3. Menampilkan Hasil
-        // Mencetak kalimat beserta nilai dari variabel 'hasil' ke konsol.
         System.out.println("Hasil penjumlahan " + angkaPertama + " + " + angkaKedua + " adalah: " + hasil);
     }
 }
